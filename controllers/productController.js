8ffdf233f7ea299ec64828c755e7b8bd541325d3
Donatien Vamuleke <donatvamuleke@gmail.com>
2022-05-29 18:40:12 +0200

    backend filtering, sorting and paginating.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,76 @@
 import Product from "../models/productModel.js";
 
+class APIFeatures {
+   constructor(query, queryString) {
+      this.query = query;
+      this.queryString = queryString;
+   }
+
+   filtering() {
+      const queryObj = { ...this.queryString }; //this.queryString = req.query
+
+      const excludedProperties = ["page", "sort", "limit"];
+
+      excludedProperties.forEach((property) => delete queryObj[property]); // the "delete" operator allows us to delete a property from an object
+
+      let queryObjStringified = JSON.stringify(queryObj);
+      queryObjStringified = queryObjStringified.replace(
+         /\b(gte|gt|lt|lte|regex)\b/g,
+         (match) => "$" + match
+      );
+
+      /**
+       * MongoBD stuff
+       * gte = greater than or equal
+       * lte = less than or equal
+       * gt = greater than
+       * lt = less than
+       */
+
+      this.query.find(JSON.parse(queryObjStringified));
+
+      return this;
+   }
+
+   sorting() {
+      if (this.queryString.sort) {
+         const sortBy = this.queryString.sort.split(",").join(" ");
+
+         this.query = this.query.sort(sortBy);
+
+         console.log("@@@@ ==> SORT", sortBy);
+      } else {
+         this.query = this.query.sort("-createdAt");
+      }
+
+      return this;
+   }
+
+   paginating() {
+      const page = this.queryString.page * 1 || 1;
+      const limit = this.queryString.limit * 1 || 9;
+      const skip = (page - 1) * limit;
+      this.query = this.query.skip(skip).limit(limit);
+
+      return this;
+   }
+}
+
 const productController = {
    getProducts: async (req, res) => {
       try {
-         const products = await Product.find({});
+         const features = new APIFeatures(Product.find({}), req.query)
+            .filtering()
+            .sorting()
+            .paginating();
 
-         return res.json(products);
+         const products = await features.query;
+
+         return res.json({
+            status: "success",
+            result: products.length,
+            products,
+         });
       } catch (err) {
          return res.status(500).json({ msg: err.message });
       }
@@ -62,14 +127,8 @@ const productController = {
    },
    updateProduct: async (req, res) => {
       try {
-         const {
-            title,
-            price,
-            description,
-            content,
-            images,
-            category,
-         } = req.body;
+         const { title, price, description, content, images, category } =
+            req.body;
 
          if (!images) {
             return res
--- models/productModel.js
+++ models/productModel.js
@@ -1,45 +1,50 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-   productId: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
+const productSchema = new mongoose.Schema(
+   {
+      productId: {
+         type: String,
+         required: true,
+         unique: true,
+         trim: true,
+      },
+      title: {
+         type: String,
+         required: true,
+         trim: true,
+      },
+      price: {
+         type: Number,
+         default: 0,
+      },
+      description: {
+         type: String,
+         required: true,
+      },
+      content: {
+         type: String,
+         required: true,
+      },
+      images: {
+         type: Object,
+         required: true,
+      },
+      category: {
+         type: String,
+         required: true,
+      },
+      checked: {
+         type: Boolean,
+         default: false,
+      },
+      sold: {
+         type: Number,
+         default: 0,
+      },
    },
-   title: {
-      type: String,
-      required: true,
-      trim: true,
-   },
-   price: {
-      type: Number,
-      default: 0,
-   },
-   description: {
-      type: String,
-      required: true,
-   },
-   content: {
-      type: String,
-      required: true,
-   },
-   images: {
-      type: Object,
-      required: true,
-   },
-   category: {
-      type: String,
-      required: true,
-   },
-   checked: {
-      type: Boolean,
-      default: false,
-   },
-   sold: {
-      type: Number,
-      default: 0,
-   },
-});
+   {
+      timestamps: true,
+   }
+);
 
 export default mongoose.model("Product", productSchema);
