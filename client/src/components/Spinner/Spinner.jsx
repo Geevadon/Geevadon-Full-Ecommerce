bd22efef4df48c3fe14f8673f25bb7bee2bfce7f
Donatien Vamuleke <donatvamuleke@gmail.com>
2022-05-31 11:15:18 +0200

    spinner styling

---
+++ client/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,24 @@
+import "./style.css";
+
+const Spinner = () => {
+   return (
+      <div className="lds-container">
+         <div className="lds-geevadon">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+         </div>
+      </div>
+   );
+};
+
+export default Spinner;
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import Spinner from "../../components/Spinner/Spinner";
 import ProductItem from "../../components/ProductItem/ProductItem";
 import { GlobalState } from "../../GlobalState";
 import "./style.css";
@@ -9,11 +10,17 @@ const Products = () => {
    const [products] = context.ProductAPI.products;
 
    return (
-      <div className="product-container">
-         {products.map((product) => (
-            <ProductItem key={product._id} product={product} />
-         ))}
-      </div>
+      <>
+         {products.length === 0 ? (
+            <Spinner />
+         ) : (
+            <div className="product-container">
+               {products.map((product) => (
+                  <ProductItem key={product._id} product={product} />
+               ))}
+            </div>
+         )}
+      </>
    );
 };
 
